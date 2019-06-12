190475da2eeba9dda16086992add9719430ff8a0
DISHA PAHUJA <pahujadisha2000@gmail.com>
2019-06-12 12:00:12 +0530

    Updated calculator

--- calculator.c
+++ calculator.c
@@ -6,20 +6,25 @@ double a, b, result;
 int choice;
 clrscr();
 result = 0;
-printf("Enter two nubers");
-scanf("%lf%lf", &a,&b);
-printf("Enter your choice: \n 1 for adition\n 2 for subtraction\n 3 for multiplication");
+printf("Enter two numbers");
+scanf("%lf %lf", &a,&b);
+printf("Enter your choice: \n 1 for adition\n 2 for subtraction\n 3 for multiplication \n4 for division\n");
 scanf("%d", &choice);
 switch(choice){
 case 1: result = a+b;
 	printf("The sum of %lf and %lf is: %lf",a,b,result);
 	break;
-case 2: result = a-b; 
+case 2: result = a-b;
 	printf("The difference of %lf and %lf is: %lf",a,b,result);
 	break;
-case 3: break;
+case 3: result=a*b;
+	printf("the product of %lf and %lf is :%lf ",a,b,result);
+	break;
+case 4: result=a/b;
+	printf("The division result of %lf and %lf is :%lf ",a,b,result);
+	break;
+
 default:"invalid choice";
 }
 getch();
 }
-
